7964cf085fa3f4713faadd9f25c49e672bd66aa6
Julio Cesar de Jesus <cesar.juliojc007@gmail.com>
2023-05-04 10:13:31 -0300

    Add variable and changed to Stateful Widget

--- lib/page/prayer_page.dart
+++ lib/page/prayer_page.dart
@@ -2,9 +2,16 @@ import 'package:app_projeto_oracoes/global/string_constants.dart';
 import 'package:app_projeto_oracoes/models/prayer.dart';
 import 'package:flutter/material.dart';
 
-class PrayerPage extends StatelessWidget {
+class PrayerPage extends StatefulWidget {
   const PrayerPage({super.key, required this.prayer});
   final Prayer prayer;
+
+  @override
+  State<PrayerPage> createState() => _PrayerPageState();
+}
+
+class _PrayerPageState extends State<PrayerPage> {
+  double textSize = 14.0;
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +19,7 @@ class PrayerPage extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: Text(
-            '${StringConstants.titleAppBarPrayer}: ${prayer.prayerTitle} ',
+            '${StringConstants.titleAppBarPrayer}: ${widget.prayer.prayerTitle}',
             style: TextStyle(
               color: Colors.amber[100],
               fontSize: 18,
@@ -21,16 +28,21 @@ class PrayerPage extends StatelessWidget {
           backgroundColor: Colors.black,
           actions: <Widget>[
             TextButton(
-              onPressed: () {},
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: const [
-                  Text('A'),
-                  Text(
-                    'A',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ],
+              onPressed: () {
+                sizedLitte();
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: const [
+                    Text('A'),
+                    Text(
+                      'A',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
@@ -45,75 +57,118 @@ class PrayerPage extends StatelessWidget {
                 ),
                 fit: BoxFit.cover),
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Center(
-                child: Column(
-                  children: [
-                    Text(
-                      prayer.prayerTitle,
-                      style: const TextStyle(
-                        fontFamily: 'Playfair_Display',
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+          child: ListView(children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        widget.prayer.prayerTitle,
+                        style: const TextStyle(
+                          fontFamily: 'Playfair_Display',
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    Text(
-                      prayer.prayerAuthor,
-                      style: TextStyle(
-                        fontFamily: 'Playfair_Display',
-                        color: Colors.red[800],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              Text(
-                prayer.prayerText,
-                style: const TextStyle(
-                  fontFamily: 'Playfair_Display',
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              Text(
-                prayer.inLatim,
-                style: const TextStyle(
-                  color: Colors.red,
-                ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              Center(
-                child: Text(
-                  prayer.latimTitle,
-                  style: const TextStyle(
-                    fontFamily: 'Playfair_Display',
-                    fontSize: 16,
-                    fontWeight: FontWeight.w800,
+                      Text(
+                        widget.prayer.prayerAuthor,
+                        style: TextStyle(
+                          fontFamily: 'Playfair_Display',
+                          fontSize: textSize,
+                          color: Colors.red[800],
+                        ),
+                      )
+                    ],
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              Text(
-                prayer.latimText,
-                style: const TextStyle(
-                  fontFamily: 'Playfair_Display',
-                  fontWeight: FontWeight.w600,
+                const SizedBox(
+                  height: 16,
                 ),
+                Text(
+                  widget.prayer.prayerText,
+                  style: TextStyle(
+                    fontFamily: 'Playfair_Display',
+                    fontSize: textSize,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  widget.prayer.inLatim,
+                  style: const TextStyle(
+                    color: Colors.red,
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Center(
+                  child: Text(
+                    widget.prayer.latimTitle,
+                    style: const TextStyle(
+                      fontFamily: 'Playfair_Display',
+                      fontSize: 16,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  widget.prayer.latimText,
+                  style: const TextStyle(
+                    fontFamily: 'Playfair_Display',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+
+  void sizedLitte() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('A'),
+              const Text(
+                'A',
+                style: TextStyle(fontSize: 18),
+              ),
+              Slider(
+                activeColor: Colors.amber,
+                inactiveColor: Colors.grey,
+                onChanged: (double newSize) {
+                  setState(() {
+                    textSize = newSize;
+                  });
+                  print(textSize);
+                },
+                value: textSize,
+                min: 10.0,
+                max: 25.0,
+              ),
+              const Text('A'),
+              const Text(
+                'A',
+                style: TextStyle(fontSize: 18),
               ),
             ],
-          ),
-        ),
+          )
+        ],
       ),
     );
   }
